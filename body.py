0534b7305edc7c6342b9629ab57b3379c2d59c71
nishii <jeewanthadeherath04@gmail.com>
2024-06-22 13:39:15 +0530

    added random food to the screen

--- body.py
+++ body.py
@@ -1,20 +1,23 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.screensize(canvwidth=600, canvheight=600)
 screen.bgcolor("black")
 screen.title("Feed me unless I will eat you!!!")
+screen.tracer(0)
+# used to turn turtle animation on or off and set a delay for update drawings
+
 snake = Snake()
+food=Food()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-screen.tracer(0)
-# used to turn turtle animation on or off and set a delay for update drawings
 
 
 is_game_on = True
