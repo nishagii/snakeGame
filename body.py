ab3aab4b3fa8419d0b2675502feaa002499bef3e
nishii <jeewanthadeherath04@gmail.com>
2024-06-26 08:07:39 +0530

    added slicing

--- body.py
+++ body.py
@@ -46,12 +46,12 @@ while is_game_on:
         scoreboard.game_over()
     # detect collision with the tail
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:#included slicing
+        if snake.head.distance(segment) < 10:
             is_game_on = False
             scoreboard.game_over()
+
+        
 
 
 screen.exitonclick()
